b41327737c5a945366196f7672ea57109fdf9331
Lugovaya Tatyana <norm.2013@yandex.ru>
2021-11-19 10:26:41 +0300

    Added new comments, changed the names of variables, removed the original variables (zeros) in the initialization of the MutableLists.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,10 +4,10 @@ fun InputInformation(): Unit {
     println("m - количество стратегий для 2-го игрока.")
     println("n: ")
     val n = readLine() //A_i
-    val NumberOfStrategies_1 = n?.toInt()
+    val numberOfStrategies_1 = n?.toInt()
     println("m: ")
     val m = readLine() //B_i
-    val NumberOfStrategies_2 = m?.toInt()
+    val numberOfStrategies_2 = m?.toInt()
     println("Введите значения выйгрышей.")
     println("* Ввод значений выигрышей осуществляется по ячейкам матрицы выигрышей:")
     println("| a   | c   | e   |")
@@ -18,33 +18,35 @@ fun InputInformation(): Unit {
     println("В порядке a -> b -> c -> d -> e -> f -> g -> h -> i -> j -> k -> l")
 
 //Тут нужно будет обрабатывать дохрена исключений по вводу. Пустой ввод, ввод не числа, можно сделать это в when. Подумать над этим.
-    val NumberOfStrategies = (NumberOfStrategies_1!! * NumberOfStrategies_2!!)*2
-    val GameMatrixValues = mutableListOf<Double?>(0.0)
-    for (element in 0 until NumberOfStrategies){
+    // Именование переменных с маленькой буквы. Это "хороший тон"
+    // через Shift + F6 можно отредактировать имя переменной во всех местах сразу. Попробуй Enter в конце
+    val numberOfStrategies = (numberOfStrategies_1!! * numberOfStrategies_2!!)*2
+    val gameMatrixValues = mutableListOf<Double?>()
+    for (element in 0 until numberOfStrategies){
         var enter = readLine()
         var New_enter = enter?.toDouble()
-        GameMatrixValues.add(element, New_enter)
+        gameMatrixValues.add(element, New_enter)
     }
-    if (GameMatrixValues.size > NumberOfStrategies-1)
-        GameMatrixValues.removeAt(NumberOfStrategies)
-    println(GameMatrixValues)
+    //if (GameMatrixValues.size > NumberOfStrategies-1)
+     //   GameMatrixValues.removeAt(NumberOfStrategies)
+    println(gameMatrixValues)
 
-    val MatrixValuesFirst = mutableListOf<Double?>(0.0)
-    val MatrixValuesSecond = mutableListOf<Double?>(0.0)
+    val matrixValuesFirst = mutableListOf<Double?>()
+    val matrixValuesSecond = mutableListOf<Double?>()
 
-    for (i in 0 until NumberOfStrategies step 2){
-        MatrixValuesFirst.add(GameMatrixValues[i])
+    for (i in 0 until numberOfStrategies step 2){
+        matrixValuesFirst.add(gameMatrixValues[i])
     }
-    for (i in 1 until NumberOfStrategies step 2){
-        MatrixValuesSecond.add(GameMatrixValues[i])
+    for (i in 1 until numberOfStrategies step 2){
+        matrixValuesSecond.add(gameMatrixValues[i])
     }
 
-    if (MatrixValuesFirst.size > 0)
-        MatrixValuesFirst.removeAt(0)
-    if (MatrixValuesSecond.size > 0)
-        MatrixValuesSecond.removeAt(0)
-    println(MatrixValuesFirst)
-    println(MatrixValuesSecond)
+    //if (MatrixValuesFirst.size > 0)
+      //  MatrixValuesFirst.removeAt(0)
+    //if (MatrixValuesSecond.size > 0)
+      //  MatrixValuesSecond.removeAt(0)
+    println(matrixValuesFirst)
+    println(matrixValuesSecond)
 }
 
 fun main(args: Array<String>) {
